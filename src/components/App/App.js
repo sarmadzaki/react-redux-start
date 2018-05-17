6e1cf44406d78cde5ba13a9cfa3fdf650cbe1c24
Sarmad Zaki <sarmad.zaki@yahoo.com>
2018-05-17 15:59:17 +0500

    working with demo post api

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from '../../logo.svg';
 import './App.css';
-
+import Todo from '../Todo/todo'
 class App extends Component {
   render() {
     return (
@@ -10,6 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React and Redux</h1>
         </header>
+        <Todo />
       </div>
     );
   }
