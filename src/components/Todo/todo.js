3760326bc3059cd7a63725e55c27511266ff7052
Sarmad Zaki <sarmad.zaki@yahoo.com>
2018-05-17 15:59:17 +0500

    connect with redux successfully

---
+++ src/components/Todo/todo.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+class Todo extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <input ref="todo" name="todo" />
+            </div>
+        )
+    }
+}
+export default connect()(Todo);
