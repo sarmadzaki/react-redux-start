02c200ac79f9a8da3bbceb387101ccd6d564a2fa
Sarmad <sarmad.zaki@yahoo.com>
2018-11-09 16:06:41 +0500

    deleting keys

--- src/config/keys.js
+++
@@ -1,9 +0,0 @@
-  // Firebase Config SDK
-  export const firebasConfig =  {
-    apiKey: "AIzaSyBEeLfRPpFnc0S9YIOSggRdXTXHe1teiFY",
-    authDomain: "mytepmlate.firebaseapp.com",
-    databaseURL: "https://mytepmlate.firebaseio.com",
-    projectId: "mytepmlate",
-    storageBucket: "",
-    messagingSenderId: "756353018862"
-  };
